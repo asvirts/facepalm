5b77ba910756032f0f745e1d37940b9c57747ab2
Andrew Virts <asvirts@outlook.com>
2023-05-07 18:49:19 -0500

    favicon

--- facepalm/src/index.js
+++ facepalm/src/index.js
@@ -12,8 +12,7 @@ const router = createBrowserRouter([
   }
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
